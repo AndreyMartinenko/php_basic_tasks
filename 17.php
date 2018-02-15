c141327f7432da86ce4f17cb3e4f5f98eb8f46e4
AndreyMartinenko <Andremartinenko@gmail.com>
2018-02-15 12:52:56 +0200

    task 17 ready to check

---
+++ 17.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 12.02.18
+ * Time: 19:03
+ */
+$a='78';
+$b=78;
+if ($a==$b) {
+    echo 'Значения равны';}
+    else echo 'Значения не равны';
+echo "<br/>";
