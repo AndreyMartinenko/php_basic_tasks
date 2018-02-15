ad7776f5fb082789268d58bae0ea5c5df8922612
AndreyMartinenko <Andremartinenko@gmail.com>
2018-02-15 17:33:33 +0200

    ready to check

---
+++ 25.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php</title>
+</head>
+<body>
+<?php
+ echo 'World';
+?>
+<p>and now is different</p>
+<?='Hello'?>
+<p>hello writed without echo command, and it called short tag </p>
+</body>
+</html>
