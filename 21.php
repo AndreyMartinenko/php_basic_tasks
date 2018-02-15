c64f507fd9b20d11c60da4ac583a0040354a179e
AndreyMartinenko <Andremartinenko@gmail.com>
2018-02-15 14:47:59 +0200

    done, like 20.php

---
+++ 21.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 15.02.18
+ * Time: 14:33
+ */
+$a = 0;
+$b = (boolean)$a;
+
+echo "число $a при переводе в булев тип изменится на '$b ' <br/>
+      так как при переводе значение boolean будет = false(по правилам приведения типов)";
+echo "<br>";
+var_dump($a, $b);
