f6a9c1cd584250e2fc644ac67decda3d47a268c5
AndreyMartinenko <Andremartinenko@gmail.com>
2018-02-15 13:02:26 +0200

    done

---
+++ 19.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 14.02.18
+ * Time: 19:12
+ */
+$a='78';
+$b=78;
+var_dump($a,$b);
+echo '<br />';
+if ($a==$b) {
+    echo 'равны';
+} else
+    echo 'не равны';
