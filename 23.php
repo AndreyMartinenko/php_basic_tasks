d539908be8d989991223b532d78b7525673f04bf
AndreyMartinenko <Andremartinenko@gmail.com>
2018-02-15 16:42:26 +0200

    done 23

---
+++ 23.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 15.02.18
+ * Time: 15:42
+ */
+;
+$c = 'third string';
+$d = 'fourth string';
+echo 'first string';
+echo "<br>";
+print 'second string';
+echo "<br>";
+echo $c;
+echo "<br>";
+printf($d);
