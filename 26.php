347d8f76d2ee8fc99bfd5059eda05ca7c039eb99
AndreyMartinenko <Andremartinenko@gmail.com>
2018-02-15 21:09:54 +0200

    two mode of define constats

---
+++ 26.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 15.02.18
+ * Time: 20:49
+ */
+//first mode
+define('DAYS_COUNT','7');
+define ('MONTH_COUNT','12');
+
+//second mode
+const DAYS_COUNT = 7;
+const MOUNTH_COUNT = 12;
+
