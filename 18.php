0bbf48d16167531b9dcf9e9b2a2dea3bbcf8d714
AndreyMartinenko <Andremartinenko@gmail.com>
2018-02-15 12:58:05 +0200

    is done

---
+++ 18.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 14.02.18
+ * Time: 19:05
+ */
+$a='78';
+$b=78;
+if ($a===$b) {
+    echo "Значения '$a' и $b : эквивалентны";
+} else
+    echo "Значения '$a' и $b : не эквивалентны";
+echo "<br/>";
