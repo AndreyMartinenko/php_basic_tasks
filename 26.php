7c433c390338da27cac949e87ffc740ebecd782a
AndreyMartinenko <Andremartinenko@gmail.com>
2018-02-15 21:21:33 +0200

    change define constants

--- 26.php
+++ 26.php
@@ -6,8 +6,8 @@
  * Time: 20:49
  */
 //first mode
-define('DAYS_COUNT','7');
-define ('MONTH_COUNT','12');
+define('DAYS_COUNT',7);
+define ('MONTH_COUNT',12);
 
 //second mode
 const DAYS_COUNT = 7;
